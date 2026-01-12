25980579a73e2a78f8e25029a199a6b02c09af5c
Ivan Samoiliuk <bessob10@gmail.com>
2026-01-13 01:50:10 +0200

    Refactor TransactionOperatorTableHeader component to improve layout by replacing fixed widths with flexible flex properties and adding new columns for balance, cash, and total.

--- src/components/TransactionOperatorTableHeader.jsx
+++ src/components/TransactionOperatorTableHeader.jsx
@@ -1,22 +1,27 @@
 export const TransactionOperatorTableHeader = () => {
     return (
       <div className="w-full h-12 flex flex-row items-center justify-between border-b-2 border-['#E9E9E9'] ">
-        <div className="w-1/5 pl-6 pr-6">
+        <div className="flex-1 pl-6 pr-6 min-w-[120px]">
           <span className=" font-bold text-[#727272]">Час та дата</span>
         </div>
-        <div className="w-1/5 ">
+        <div className="flex-1 min-w-[100px]">
           <span className=" font-bold text-[#727272]">AЗС</span>
         </div>
-        <div className="w-1/5 pl-6 pr-6">
+        <div className="flex-1 pl-6 pr-6 min-w-[100px]">
           <span className=" font-bold text-[#727272]">Тип палива</span>
         </div>
-        <div className="w-1/5">
+        <div className="flex-1 min-w-[80px]">
           <span className=" font-bold text-[#727272]">Літри</span>
         </div>
-        <div className="w-1/5">
-          <span className=" font-bold text-[#727272]">Сума</span>
+        <div className="flex-1 min-w-[80px]">
+          <span className=" font-bold text-[#727272]">Баланс</span>
         </div>
-        
+        <div className="flex-1 min-w-[80px]">
+          <span className=" font-bold text-[#727272]">Готівка</span>
+        </div>
+        <div className="flex-1 min-w-[80px]">
+          <span className=" font-bold text-[#727272]">Всього</span>
+        </div>
       </div>
     );
   };
