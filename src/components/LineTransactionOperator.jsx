a89725d1051aacc8636f0f5c45b42e543a5ac33b
Ivan Samoiliuk <bessob10@gmail.com>
2026-01-13 01:49:47 +0200

    Enhance LineTransactionOperator layout by utilizing flexible flex properties, ensuring consistent minimum widths, and improving monetary value formatting for better readability.

--- src/components/LineTransactionOperator.jsx
+++ src/components/LineTransactionOperator.jsx
@@ -25,10 +25,10 @@ export const LineTransactionOperator = ({ data}) => {
 
   return (
     <div className="w-full h-max flex flex-row items-center justify-between border-b border-['#E9E9E9'] py-[8px]">
-      <div className="w-1/5 pl-6 pr-6">
+      <div className="flex-1 pl-6 pr-6 min-w-[120px]">
         <span>{`${format(new Date(data.requestDate), 'HH:mm:ss dd.MM.yyy') }`}</span>
       </div>
-      <div className="w-1/5">
+      <div className="flex-1 min-w-[100px]">
         <div>
           <span
         >
@@ -36,14 +36,20 @@ export const LineTransactionOperator = ({ data}) => {
         </span>
         </div>
       </div>
-      <div className="w-1/5 overflow-hidden pl-6 pr-6 text-ellipsis">
+      <div className="flex-1 overflow-hidden pl-6 pr-6 text-ellipsis min-w-[100px]">
         <span className=' whitespace-nowrap '>{getProductName()}</span>
       </div>
-      <div className="w-1/5">
+      <div className="flex-1 min-w-[80px]">
         <span>{`${data.litrs} л`}</span>
       </div>
-      <div className="w-1/5">
-        <span>{`${data.sum} грн`}</span>
+      <div className="flex-1 min-w-[80px]">
+        <span>{`${(+data.sum || 0).toFixed(2)} грн`}</span>
+      </div>
+      <div className="flex-1 min-w-[80px]">
+        <span>{`${(+data.additionalPayment || 0).toFixed(2)} грн`}</span>
+      </div>
+      <div className="flex-1 min-w-[80px]">
+        <span>{`${(+data.total || +data.sum || 0).toFixed(2)} грн`}</span>
       </div>
     </div>
   );
